5b6f3c1103f017e8830422fe30965d7b43a31b1b
Erick Matsen <matsen@fhcrc.org>
2014-12-04 16:08:02 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# All Hail Sage!
+
+A few useful function definitions for <http://sagemath.org/>.
---
+++ nb-fun.py
@@ -0,0 +1,20 @@
+import csv
+import os
+from IPython.display import display, Image
+from sage.matrix.constructor import Matrix
+from sage.misc.temporary_file import tmp_dir, graphics_filename
+
+
+dirname = tmp_dir()
+
+
+def nb_show(graphics_object, ext='png'):
+    name = os.path.join(dirname, graphics_filename(ext))
+    graphics_object.save(name)
+    display(Image(name))
+
+
+def matrix_of_csv(fname):
+    with open(fname, 'rb') as csvfile:
+        mreader = csv.reader(csvfile, delimiter=',', quotechar="'")
+        return Matrix([[int(x) for x in row] for row in mreader])
